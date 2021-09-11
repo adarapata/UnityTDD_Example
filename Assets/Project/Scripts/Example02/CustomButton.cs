ef2359306f41b992070d0fae1a4e1a2922c5e403
いも <adarapata@e-f-b.jp>
2021-09-12 00:44:05 +0900

    簡易のボタンを作成

---
+++ Assets/Project/Scripts/Example02/CustomButton.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class CustomButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+{
+    [SerializeField] private Animator _animator;
+    [SerializeField] private float _duration;
+    
+    public event Action Tap;
+    public event Action LongPress;
+    private Coroutine _countDownCoroutine;
+
+    private float _downTime;
+
+    private void Awake()
+    {
+        LongPress += () => print("long!!!");
+        Tap += () => print("tap!");
+    }
+
+    public void OnPointerDown(PointerEventData eventData)
+    {
+        _downTime = Time.time;
+        _animator.SetTrigger("Pressed");
+        _countDownCoroutine = StartCoroutine(CountDownLongPress(_duration));
+    }
+
+    public void OnPointerUp(PointerEventData eventData)
+    {
+        if (Time.time - _downTime < 1F)
+        {
+            Tap?.Invoke();
+        }
+        StopCoroutine(_countDownCoroutine);
+        _animator.SetTrigger("Released");
+    }
+
+    private IEnumerator CountDownLongPress(float duration)
+    {
+        yield return new WaitForSeconds(duration);
+        LongPress?.Invoke();
+    }
+}

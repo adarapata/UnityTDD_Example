342d0b5fb325f8d9d29365761fd7d646dfabe44f
いも <adarapata@e-f-b.jp>
2021-09-12 16:06:10 +0900

    ファイル移動

---
+++ Assets/Project/Scripts/Example01/UserNameChangeView.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class UserNameChangeView : MonoBehaviour
+{
+    [SerializeField] private Text _resultText;
+
+    [SerializeField] private Button _button;
+
+    [SerializeField] private InputField _inputField;
+
+    [SerializeField] private Text _nameLabel;
+}
